87fc34bbd3b9abf440f7524b0052bb6b811b0c0c
Dheeraj <dheerajreddy.r24@gmail.com>
2022-07-22 12:14:02 +0530

    Added bottom navigation menu and fragments are added and have been linked to the menu items.

--- app/src/main/java/com/hasher/actress4kimages/MainActivity.java
+++ app/src/main/java/com/hasher/actress4kimages/MainActivity.java
@@ -1,14 +1,56 @@
 package com.hasher.actress4kimages;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationView = findViewById(R.id.bottm_nav_menu);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+
+                    case R.id.nav_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.nav_category:
+                        fragment = new CategoryFragment();
+                        break;
+                    case R.id.nav_favourite:
+                        fragment = new FavouriteFragment();
+                        break;
+
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, fragment).commit();
+
+                return true;
+            }
+        });
+
     }
 }
